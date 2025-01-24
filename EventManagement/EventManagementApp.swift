a8cbfc2f442da9c9a260728e8cdffdf8b746470d
Devashish2106 <devghanshani4@gmail.com>
2025-01-24 23:29:08 +0530

    Initial Commit

---
+++ EventManagement/EventManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventManagementApp.swift
+//  EventManagement
+//
+//  Created by Devashish Ghanshani on 24/01/25.
+//
+
+import SwiftUI
+
+@main
+struct EventManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
